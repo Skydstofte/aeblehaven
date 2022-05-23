2f86d60263d7ca41e3da62ff0629a8d845c76076
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-05-23 09:24:01 +0200

    Latest

---
+++ js/history.js
@@ -0,0 +1,16 @@
+const nav = document.getElementById('logo-container');
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    console.log(window.innerWidth)
+    if (window.innerWidth > 480) return
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add("logo-container-minimized");
+        console.log("up")
+    } else {
+        nav.classList.remove("logo-container-minimized");
+        console.log("down")
+    }
+    lastScrollY = window.scrollY;
+});
+
