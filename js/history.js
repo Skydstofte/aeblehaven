4c73a0fe656f9689878bb0979a8f72f89535331e
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-05-27 16:45:42 +0200

    Rikke

--- js/history.js
+++ js/history.js
@@ -1,15 +1,24 @@
 const nav = document.getElementById('logo-container');
 let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-    console.log(window.innerWidth)
-    if (window.innerWidth > 480) return
-    if (lastScrollY < window.scrollY) {
-        nav.classList.add("logo-container-minimized");
-        console.log("up")
-    } else {
-        nav.classList.remove("logo-container-minimized");
-        console.log("down")
-    }
-    lastScrollY = window.scrollY;
-});
+// Anti throttle for browsers without in built anti throttle
+let timeout;
+
+window.addEventListener('scroll', function (event) {
+	// If there's a timer, cancel it
+	if (timeout) {
+		window.cancelAnimationFrame(timeout);
+	}
+
+	// Setup the new requestAnimationFrame()
+	timeout = window.requestAnimationFrame(function () {
+        if (window.innerWidth > 480) return
+        if (lastScrollY < window.scrollY) {
+            nav.classList.add("logo-container-minimized");
+        } else {
+            nav.classList.remove("logo-container-minimized");
+        }
+        lastScrollY = window.scrollY;
+    });
+
+}, false);
