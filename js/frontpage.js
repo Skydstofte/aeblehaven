1c8b82537991cd05ff1cbf7a360d6d63c787e25d
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-05-28 17:32:03 +0200

    Rikke

---
+++ js/frontpage.js
@@ -0,0 +1,13 @@
+let message = document.getElementById('taleboble-one');
+setInterval(function(){
+    changingDiv();
+},5*6000);
+
+function changingDiv(){
+  if(message.style.display == 'none' || message.style.display == ""){
+    message.style.display = 'block';
+  }
+  else{
+    message.style.display = 'none';
+  }
+}
