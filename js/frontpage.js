4f7bac84aa4f2e23d9c7cb9a49ddb6119d37ff64
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-05-29 17:26:12 +0200

    Rikke

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,13 +1,18 @@
-// let message = document.getElementById('taleboble-one');
-// setInterval(function(){
-//     changingDiv();
-// },5*6000);
+let message = document.getElementById('taleboble-one');
+let messageTwo = document.getElementById('taleboble-two');
+let messageSwapper = true;
 
-// function changingDiv(){
-//   if(message.style.display == 'none' || message.style.display == ""){
-//     message.style.display = 'block';
-//   }
-//   else{
-//     message.style.display = 'none';
-//   }
-// }
+swapMessage();
+
+function swapMessage(){
+  setInterval(function(){
+    messageSwapper = !messageSwapper;
+    if(messageSwapper){
+      message.style.display = 'block';
+      messageTwo.style.display = 'none';
+    } else {
+      message.style.display = 'none';
+      messageTwo.style.display = 'block';
+    }
+  }, 10000);
+}
