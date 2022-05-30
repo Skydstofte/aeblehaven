beccfe92df44aa42b9f3d8cd0d7903d48b7aee32
MicheleDepenau <eaamede@students.eaaa.dk>
2022-05-30 10:23:58 +0200

    Michele

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,15 @@
-// Arbejder på en js løsning til mouseover istedet for løsningen ved css, blot for øvelse, da den ikke findes nødvendig
+// Arbejder på en js løsning til mouseover istedet for 
+// løsningen ved css, blot for øvelse, da den ikke findes nødvendig!
 
-// const el = document.getElementById('det-salte');
+// const detSalte = document.getElementById('det-salte');
 
-// const hiddenEl = document.getElementById('det-salte-list');
+// const detSalteList = document.getElementById('det-salte-list');
 
 // // Show hidden DIV on hover
-// el.addEventListener('mouseover', function handleMouseOver() {
-//   hiddenEl.style.display = 'block';
-// }
-// el.addEventListener('mouseout', function handleMouseOut() {
-//     hiddenEl.style.visibility = 'hidden';
-//   }
+// detSalte.addEventListener('mouseover', function handleMouseOver() {
+//   detSalteList.style.display = 'flex';
+// })
+
+// detSalte.addEventListener('mouseout', function handleMouseOut() {
+//     detSalteList.style.display = 'none';
+// })
