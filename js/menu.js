be992f2566de8225664a1bc06923c7f060324adc
MicheleDepenau <eaamede@students.eaaa.dk>
2022-05-30 09:39:42 +0200

    Michele

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+// Arbejder på en js løsning til mouseover istedet for løsningen ved css, blot for øvelse, da den ikke findes nødvendig
+
+// const el = document.getElementById('det-salte');
+
+// const hiddenEl = document.getElementById('det-salte-list');
+
+// // Show hidden DIV on hover
+// el.addEventListener('mouseover', function handleMouseOver() {
+//   hiddenEl.style.display = 'block';
+// }
+// el.addEventListener('mouseout', function handleMouseOut() {
+//     hiddenEl.style.visibility = 'hidden';
+//   }
