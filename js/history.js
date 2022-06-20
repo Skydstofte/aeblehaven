1829a1106fc5c82e826f292b18e3ae8314d9600f
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-06-20 10:03:17 +0200

    Rikke

--- js/history.js
+++ js/history.js
@@ -1,17 +1,30 @@
-let extraText = document.querySelector('.history-container-content-textbox-extratext')
-let moreBtn = document.querySelector('.history-container-content-textbox-morebutton')
-let lessBtn = document.querySelector('.history-container-content-textbox-lessbutton')
+let text1 = document.getElementById('text1')
+let btn1 = document.getElementById('btn1')
+btn1.addEventListener('click', function(){toggleText(btn1, text1)});  
 
-moreBtn.onclick = function() {
-    extraText.style.display = 'block';
-    // remove "more" and add "less"
-    moreBtn.style.display = 'none';
-    lessBtn.style.display = 'block';
-};
+let text2 = document.getElementById('text2')
+let btn2 = document.getElementById('btn2')
+btn2.addEventListener('click', function(){toggleText(btn2, text2)});  
 
-lessBtn.onclick = function() {
-    extraText.style.display = 'none';
-    // remove "less" and add "more"
-    lessBtn.style.display = 'none';
-    moreBtn.style.display = 'block';
-};
+let text3 = document.getElementById('text3')
+let btn3 = document.getElementById('btn3')
+btn3.addEventListener('click', function(){toggleText(btn3, text3)});  
+
+let text4 = document.getElementById('text4')
+let btn4 = document.getElementById('btn4')
+btn4.addEventListener('click', function(){toggleText(btn4, text4)});  
+
+let text5 = document.getElementById('text5')
+let btn5 = document.getElementById('btn5')
+btn5.addEventListener('click', function(){toggleText(btn5, text5)});  
+
+function toggleText(button, text){
+    // Hvis (sandt/falsk udtryk) -> så(?) -kør kode x- ellers(:) -kør kode y- -------- (ternary operator/conditional expression)
+    button.textContent == 'Vis mere' ? button.textContent = 'Vis mindre' : button.textContent = 'Vis mere'
+    button.textContent == 'Vis mindre' ? text.style.display = 'block' : text.style.display = 'none'
+    // if(button.style.display == 'none'){
+    //     button.style.display = 'block'
+    // } else {
+    //     button.style.display = 'none'
+    // }
+}
